be8b89ad19b5aad460b37312472a90fed77564ba
Tyler Martin <tylertmartin2020@gmail.com>
2023-04-23 10:56:52 -0400

    Initial Commit

---
+++ MovieCompanion/MovieCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCompanionApp.swift
+//  MovieCompanion
+//
+//  Created by Tyler on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
